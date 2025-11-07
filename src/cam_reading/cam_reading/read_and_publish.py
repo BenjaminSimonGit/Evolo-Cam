7b5a10cdf88c42040560dfc7c23cb53976625301
BenjaminSimonGit <BenjaminSimon.studie@gmail.com>
2025-11-07 11:23:44 +0000

    small fix

--- src/cam_reading/cam_reading/read_and_publish.py
+++ src/cam_reading/cam_reading/read_and_publish.py
@@ -25,7 +25,7 @@ class GimbalReadAndPublish(Node):
 
         # Create publisher
         self.publisher_ = self.create_publisher(Vector3, 'gimbal_euler', 10)
-        timer_period = 3  # seconds
+        timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
         # Create subscriber
